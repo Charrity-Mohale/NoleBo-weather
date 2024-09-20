d5df6723ed20a56afbcba1ece0d054ab7abd6a11
Charrity <ntswakimohale@gmail.com>
2024-09-20 20:59:13 +0200

    injected additional weather data

--- src/index.js
+++ src/index.js
@@ -1,38 +1,55 @@
-function updateWeather (response) {
-    let temperature = document.querySelector("#current-temperature");
-    temperature.innerHTML = Math.round(response.data.temperature.current);
-    let condition = document.querySelector("#condition");
-    condition.innerHTML = response.data.condition.description;
-    let humidity = document.querySelector("#humidity");
-    humidity.innerHTML = response.data.temperature.humidity + "%";
-    let windSpeed = document.querySelector("#wind");
-    windSpeed.innerHTML = response.data.wind.speed + "km/h";
-    console.log(response.data);
+function updateWeather(response) {
+  let temperature = document.querySelector("#current-temperature");
+  let condition = document.querySelector("#condition");
+  let humidity = document.querySelector("#humidity");
+  let windSpeed = document.querySelector("#wind");
+  let city = document.querySelector("#city");
+  let time = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
-    let city = document.querySelector("#city");
-    city.innerHTML = response.data.city;
+  console.log(response.data);
+
+  temperature.innerHTML = Math.round(response.data.temperature.current);
+  condition.innerHTML = response.data.condition.description;
+  humidity.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeed.innerHTML = `${response.data.wind.speed}km/h`;
+  time.innerHTML = formatDate(date);
+  city.innerHTML = response.data.city;
 }
 
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${date.getHours()}:${date.getMinutes()}`;
+}
 
 function searchCity(city) {
-    let apiKey = "7f04b746839fb9480ae129200267toa1";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`
-    axios.get(apiUrl).then(updateWeather);
+  let apiKey = "7f04b746839fb9480ae129200267toa1";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(updateWeather);
 }
 
-
-
 function searchCityFunction(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-form-input");
-    let city = document.querySelector("#city");
-    city.innerHTML = searchInput.value;
-    searchCity(searchInput.value);
-    
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let city = document.querySelector("#city");
+  city.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", searchCityFunction);
-
-
