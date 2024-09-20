d9cab5c2615e0b0a0b5d71e6acd934fc3ed144f2
Charrity <ntswakimohale@gmail.com>
2024-09-20 18:32:08 +0200

    Added a search engine

---
+++ src/index.js
@@ -0,0 +1,12 @@
+function searchCity(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+    let city = document.querySelector("#city");
+    city.innerHTML = searchInput.value;
+    
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", searchCity);
+
+
